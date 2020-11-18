a9a292d19dbe178fc163ebc6249c5644094c8779
jmurrii <jonmurrii@gmail.com>
2020-11-18 06:55:56 +0000

    Added Stripe Elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HokDOALeWFJeO4Ayh1QF40Zs2wPTdzgwzGspz8cfuhaKu03oZoGNgxr3DK9YkhKZDvRGeCSUt2qU9ADxr1jNf5500eJ7Dcz5q',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
