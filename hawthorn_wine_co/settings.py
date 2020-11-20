e17ab1465f2ee737481a3bceb8f1f7825ea52353
jmurrii <jonmurrii@gmail.com>
2020-11-20 13:56:02 +0000

    Removed secret key and set debug

--- hawthorn_wine_co/settings.py
+++ hawthorn_wine_co/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cs993(=4d**l3!_&awd39!z)1j+)^l$f5s5uu*$2b_@h0dzzpr'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
