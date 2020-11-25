7c086ced9f5e40adb317e7f3a7b1849f004f8b53
jmurrii <jonmurrii@gmail.com>
2020-11-25 09:30:28 +0000

    Improved responsivesness of footer elements

---
+++ static/js/age-verification.js
@@ -0,0 +1,10 @@
+var ageCheck = window.prompt("Please enter your age")
+
+if (ageCheck < 18) {
+    window.alert("You must be at least 18 years to enter this site");
+    location.href(http:)
+}
+else if (ageCheck > 18 && ageCheck < 101) {
+    window.alert("Thank You, Welcome to Hawthorn Wines Co.")
+}
+
