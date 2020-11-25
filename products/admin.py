75c4a0468e7091d733071c23c42b16dd05ef2085
jmurrii <jonmurrii@gmail.com>
2020-11-25 17:21:26 +0000

    Added subcategory Countries to product.model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Country
 
 # Register your models here.
 
@@ -29,6 +29,13 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Country, CountryAdmin)
--- products/models.py
+++ products/models.py
@@ -16,6 +16,21 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Country(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Countries'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
