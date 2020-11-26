8a603271d65ed758427fe664d4622392b808f60c
jmurrii <jonmurrii@gmail.com>
2020-11-26 09:20:56 +0000

    Added landing page age verification modal

--- static/js/age-verification.js
+++ static/js/age-verification.js
@@ -1,10 +1,3 @@
-var ageCheck = window.prompt("Please enter your age")
-
-if (ageCheck < 18) {
-    window.alert("You must be at least 18 years to enter this site");
-    location.href(http:)
-}
-else if (ageCheck > 18 && ageCheck < 101) {
-    window.alert("Thank You, Welcome to Hawthorn Wines Co.")
-}
-
+$(window).on('load',function(){
+$('#ageID').modal('show');
+});
